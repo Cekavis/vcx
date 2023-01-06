a344b879d85ec010e6913ac3ccc28a9816812378
Cekavis <44022226+Cekavis@users.noreply.github.com>
2023-01-06 21:25:15 +0800

    Width of line, rect, and line

--- src/VCX/Labs/Project/RenderSVG.cpp
+++ src/VCX/Labs/Project/RenderSVG.cpp
@@ -20,12 +20,62 @@ namespace VCX::Labs::Project {
         image = Common::CreatePureImageRGB(width, height, { 1., 1., 1. });
 
         for (auto child = root->FirstChildElement(); child != NULL; child = child->NextSiblingElement()) {
-            if (child->Name() == std::string("path"))
-                DrawPath(image, child);
+            if (child->Name() == std::string("line"))
+                DrawLine(image, child);
+            if (child->Name() == std::string("rect"))
+                DrawRect(image, child);
             if (child->Name() == std::string("polygon"))
                 DrawPolygon(image, child);
+            if (child->Name() == std::string("path"))
+                DrawPath(image, child);
         }
         return 1;
+    }
+
+    void DrawLine(ImageRGB &image, const tinyxml2::XMLElement *path) {
+        float x1, y1, x2, y2;
+        if (path->QueryFloatAttribute("x1", &x1)) return;
+        if (path->QueryFloatAttribute("y1", &y1)) return;
+        if (path->QueryFloatAttribute("x2", &x2)) return;
+        if (path->QueryFloatAttribute("y2", &y2)) return;
+        x1 /= ratio, y1 /= ratio, x2 /= ratio, y2 /= ratio;
+
+        glm::vec4 color = GetColor(path->Attribute("stroke"));
+        float width = path->FloatAttribute("stroke-width", 0);
+        if (color.a > 0)
+            if (width > 0)
+                _drawThickLine(image, color, { x1, y1 }, { x2, y2 }, width);
+            else
+                _drawLine(image, color, { x1, y1 }, { x2, y2 });
+    }
+
+    void DrawRect(ImageRGB &image, const tinyxml2::XMLElement *path) {
+        float x, y, w, h;
+        if (path->QueryFloatAttribute("x", &x)) return;
+        if (path->QueryFloatAttribute("y", &y)) return;
+        if (path->QueryFloatAttribute("width", &w)) return;
+        if (path->QueryFloatAttribute("height", &h)) return;
+        x /= ratio, y /= ratio, w /= ratio, h /= ratio;
+
+        /* Draw interior */
+        glm::vec4 color = GetColor(path->Attribute("fill"));
+        if (color.a > 0)
+            _drawPolygonFilled(image, color, {
+                { x, y },
+                { x + w, y },
+                { x + w, y + h },
+                { x, y + h }
+            });
+        
+        /* Draw outline */
+        color = GetColor(path->Attribute("stroke"));
+        float width = path->FloatAttribute("stroke-width", 1);
+        if (color.a > 0) {
+            _drawThickLine(image, color, { x - width, y }, { x + w + width, y }, width);
+            _drawThickLine(image, color, { x - width, y + h }, { x + w + width, y + h }, width);
+            _drawThickLine(image, color, { x + w, y }, { x + w, y + h }, width);
+            _drawThickLine(image, color, { x, y + h }, { x, y }, width);
+        }
     }
 
     void DrawPolygon(ImageRGB &image, const tinyxml2::XMLElement *path) {
@@ -37,9 +87,21 @@ namespace VCX::Labs::Project {
         for (auto &p : points) p /= ratio;
         points.push_back(points[0]);
         int n = points.size() - 1;
-        for (int i = 0; i < n; i++)
-            _drawLine(image, GetColor(path->Attribute("stroke")), points[i], points[i+1]);
-        _drawPolygonFilled(image, GetColor(path->Attribute("fill")), points);
+
+        /* Draw interior */
+        glm::vec4 color = GetColor(path->Attribute("fill"));
+        if (color.a > 0)
+            _drawPolygonFilled(image, color, points);
+
+        /* Draw outline */
+        color = GetColor(path->Attribute("stroke"));
+        float width = path->FloatAttribute("stroke-width", 0);
+        if (color.a > 0)
+            for (int i = 0; i < n; i++)
+                if (width > 0)
+                    _drawThickLine(image, color, points[i], points[i + 1], width);
+                else
+                    _drawLine(image, color, points[i], points[i + 1]);
     }
 
     void DrawPath(ImageRGB &image, const tinyxml2::XMLElement *path) {
@@ -114,6 +176,7 @@ namespace VCX::Labs::Project {
         ImageRGB &                     canvas,
         glm::vec3 const                color,
         std::vector<glm::vec2> const & points) {
+
         int n = points.size() - 1;
         for (int x = 0; x < width; ++x) {
             std::vector<int> ys;
@@ -130,6 +193,23 @@ namespace VCX::Labs::Project {
                 for (int y = ys[i]; y <= ys[i + 1]; ++y)
                     canvas.SetAt({ (std::size_t)x, (std::size_t)y }, color);
         }
+    }
+
+    void _drawThickLine(
+        ImageRGB &       canvas,
+        glm::vec3 const  color,
+        glm::vec2 const p0,
+        glm::vec2 const p1,
+        float            width) {
+        
+        std::vector<glm::vec2> points;
+        glm::vec2 normal = glm::normalize(glm::vec2(p1.y - p0.y, p0.x - p1.x));
+        points.push_back(p0 + normal * width);
+        points.push_back(p0 - normal * width);
+        points.push_back(p1 - normal * width);
+        points.push_back(p1 + normal * width);
+        points.push_back(p0 + normal * width);
+        _drawPolygonFilled(canvas, color, points);
     }
 
     std::vector<glm::vec2> ParsePoints(const char *s){
@@ -151,32 +231,51 @@ namespace VCX::Labs::Project {
             points.push_back({numbers[i], numbers[i + 1]});
         return points;
     }
+    
+    glm::vec4 GetColorFromHex(const char *s){
+        int r = std::stoi(std::string(s    , s + 2), 0, 16);
+        int g = std::stoi(std::string(s + 2, s + 4), 0, 16);
+        int b = std::stoi(std::string(s + 4, s + 6), 0, 16);
+        return {r / 255.0f, g / 255.0f, b / 255.0f, 1};
+    }
 
-    glm::vec3 GetColor(const char *s){
-        if (s == NULL) return {0, 0, 0};
+    glm::vec4 GetColor(const char *s){
+        std::cerr << "GetColor: " << s << "\n";
+        if (s == NULL) return glm::vec4(0);
         if (s[0] == '#') {
-            if (strlen(s) == 7) {
-                int r = std::stoi(std::string(s + 1, s + 3), 0, 16);
-                int g = std::stoi(std::string(s + 3, s + 5), 0, 16);
-                int b = std::stoi(std::string(s + 5, s + 7), 0, 16);
-                return {r / 255.0f, g / 255.0f, b / 255.0f};
+            if (strlen(s) == 7){
+                bool ok = true;
+                for (int i = 1; i < 7; ++i)
+                    if (!isxdigit((unsigned char)s[i]))
+                        ok = false;
+                if (ok) return GetColorFromHex(s + 1);
             }
-            else {
-                std::cerr << "GetColor: invalid color string\n";
-                return {0, 0, 0};
-            }
+            std::cerr << "GetColor: invalid color string\n";
+            return glm::vec4(0);
         }
-        else if (strcmp(s, "red") == 0) return {1, 0, 0};
-        else if (strcmp(s, "green") == 0) return {0, 1, 0};
-        else if (strcmp(s, "blue") == 0) return {0, 0, 1};
-        else if (strcmp(s, "yellow") == 0) return {1, 1, 0};
-        else if (strcmp(s, "cyan") == 0) return {0, 1, 1};
-        else if (strcmp(s, "magenta") == 0) return {1, 0, 1};
-        else if (strcmp(s, "white") == 0) return {1, 1, 1};
-        else if (strcmp(s, "black") == 0) return {0, 0, 0};
+        /* See https://developer.mozilla.org/en-US/docs/Web/CSS/named-color */
+        else if (strcmp(s, "black"  ) == 0) return GetColorFromHex("000000");
+        else if (strcmp(s, "silver" ) == 0) return GetColorFromHex("c0c0c0");
+        else if (strcmp(s, "gray"   ) == 0) return GetColorFromHex("808080");
+        else if (strcmp(s, "white"  ) == 0) return GetColorFromHex("ffffff");
+        else if (strcmp(s, "maroon" ) == 0) return GetColorFromHex("800000");
+        else if (strcmp(s, "red"    ) == 0) return GetColorFromHex("ff0000");
+        else if (strcmp(s, "purple" ) == 0) return GetColorFromHex("800080");
+        else if (strcmp(s, "fuchsia") == 0) return GetColorFromHex("ff00ff");
+        else if (strcmp(s, "green"  ) == 0) return GetColorFromHex("008000");
+        else if (strcmp(s, "lime"   ) == 0) return GetColorFromHex("00ff00");
+        else if (strcmp(s, "olive"  ) == 0) return GetColorFromHex("808000");
+        else if (strcmp(s, "yellow" ) == 0) return GetColorFromHex("ffff00");
+        else if (strcmp(s, "navy"   ) == 0) return GetColorFromHex("000080");
+        else if (strcmp(s, "blue"   ) == 0) return GetColorFromHex("0000ff");
+        else if (strcmp(s, "teal"   ) == 0) return GetColorFromHex("008080");
+        else if (strcmp(s, "aqua"   ) == 0) return GetColorFromHex("00ffff");
+        else if (strcmp(s, "orange" ) == 0) return GetColorFromHex("ffa500");
+
+        else if (strcmp(s, "transparent") == 0) return glm::vec4(0);
         else {
             std::cerr << "GetColor: invalid color string\n";
-            return {0, 0, 0};
+            return {0, 0, 0, 0};
         }
     }
 }
--- src/VCX/Labs/Project/RenderSVG.h
+++ src/VCX/Labs/Project/RenderSVG.h
@@ -8,13 +8,17 @@ using VCX::Labs::Common::ImageRGB;
 namespace VCX::Labs::Project {
     bool render(ImageRGB &image, const tinyxml2::XMLElement *root, std::uint32_t &width, std::uint32_t &height);
 
+    void DrawLine(ImageRGB &image, const tinyxml2::XMLElement *path);
+    void DrawRect(ImageRGB &image, const tinyxml2::XMLElement *path);
     void DrawPolygon(ImageRGB &image, const tinyxml2::XMLElement *path);
     void DrawPath(ImageRGB &image, const tinyxml2::XMLElement *path);
 
     void _drawLine(Common::ImageRGB &, glm::vec3, glm::ivec2, glm::ivec2);
     void _drawTriangleFilled(Common::ImageRGB &, glm::vec3, glm::ivec2, glm::ivec2, glm::ivec2);
     void _drawPolygonFilled(Common::ImageRGB &, glm::vec3, std::vector<glm::vec2> const &);
+    void _drawThickLine(Common::ImageRGB &, glm::vec3, glm::vec2, glm::vec2, float);
 
     std::vector<glm::vec2> ParsePoints(const char *s);
-    glm::vec3 GetColor(const char *s);
+    glm::vec4 GetColorFromHex(const char *s);
+    glm::vec4 GetColor(const char *s);
 }
